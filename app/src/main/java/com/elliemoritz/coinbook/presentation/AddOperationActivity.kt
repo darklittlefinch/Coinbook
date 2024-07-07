386754c310684d6d3f46e265d2ee16583496db19
Elisa Moritz <dark.little.finch@gmail.com>
2024-07-08 02:20:08 +0700

    feat: add navigation between MainActivity and OperationsActivity

--- app/src/main/java/com/elliemoritz/coinbook/presentation/AddOperationActivity.kt
+++ app/src/main/java/com/elliemoritz/coinbook/presentation/AddOperationActivity.kt
@@ -1,5 +1,6 @@
 package com.elliemoritz.coinbook.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/elliemoritz/coinbook/presentation/MainActivity.kt
+++ app/src/main/java/com/elliemoritz/coinbook/presentation/MainActivity.kt
@@ -1,20 +1,48 @@
-package com.elliemoritz.coinbook
+package com.elliemoritz.coinbook.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.elliemoritz.coinbook.R
+import com.elliemoritz.coinbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        setExpensesListener()
+        setIncomeListener()
+    }
+
+    private fun setIncomeListener() {
+        binding.cvIncome.setOnClickListener {
+            val intent = OperationsActivity.newIntent(this, OperationsActivity.INCOME_MODE)
+            startActivity(intent)
+        }
+    }
+
+    private fun setExpensesListener() {
+        binding.cvExpenses.setOnClickListener {
+            val intent = OperationsActivity.newIntent(this, OperationsActivity.EXPENSE_MODE)
+            startActivity(intent)
         }
     }
 }
