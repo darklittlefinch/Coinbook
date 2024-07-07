aab78e80b4df0d83bee2ac7494c304f1584db2bd
Elisa Moritz <dark.little.finch@gmail.com>
2024-07-08 01:18:59 +0700

    refactor: add clean structure

--- app/src/main/java/com/elliemoritz/coinbook/presentation/AddOperationFragment.kt
+++ app/src/main/java/com/elliemoritz/coinbook/presentation/AddOperationFragment.kt
@@ -1,15 +1,11 @@
-package com.elliemoritz.coinbook
+package com.elliemoritz.coinbook.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import com.elliemoritz.coinbook.R
 
 /**
  * A simple [Fragment] subclass.
@@ -46,6 +42,12 @@ class AddOperationFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment AddOperationFragment.
          */
+
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
