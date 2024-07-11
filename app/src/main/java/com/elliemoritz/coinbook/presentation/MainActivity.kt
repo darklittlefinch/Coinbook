d4a1d4c9fea008b439ae8c858b71201eec4bdc85
Elisa Moritz <dark.little.finch@gmail.com>
2024-07-12 02:33:58 +0700

    refactor: rename onClickListeners methods to more convenient

--- app/src/main/java/com/elliemoritz/coinbook/presentation/MainActivity.kt
+++ app/src/main/java/com/elliemoritz/coinbook/presentation/MainActivity.kt
@@ -24,94 +24,94 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        setListeners()
+        setOnClickListeners()
     }
 
-    private fun setListeners() {
-        setBalanceListener()
-        setIncomeListener()
-        setPlusListener()
-        setExpensesListener()
-        setMinusListener()
-        setMoneyBoxListener()
-        setDebtsListener()
-        setLimitsListener()
-        setAlarmsListener()
-        setHistoryListener()
-        setSettingsListener()
+    private fun setOnClickListeners() {
+        setOnBalanceClickListener()
+        setOnIncomeClickListener()
+        setOnPlusClickListener()
+        setOnExpensesClickListener()
+        setOnMinusClickListener()
+        setOnMoneyBoxClickListener()
+        setOnDebtsClickListener()
+        setOnLimitsClickListener()
+        setOnAlarmsClickListener()
+        setOnHistoryClickListener()
+        setOnSettingsClickListener()
     }
 
-    private fun setBalanceListener() {
+    private fun setOnBalanceClickListener() {
         binding.cvBalance.setOnClickListener {
             val intent = OperationsActivity.newIntent(this, OperationsActivity.MODE_BALANCE)
             startActivity(intent)
         }
     }
 
-    private fun setIncomeListener() {
+    private fun setOnIncomeClickListener() {
         binding.cvIncome.setOnClickListener {
             val intent = IncomeActivity.newIntent(this)
             startActivity(intent)
         }
     }
 
-    private fun setPlusListener() {
+    private fun setOnPlusClickListener() {
         binding.cvPlus.setOnClickListener {
             val intent = OperationsActivity.newIntent(this, OperationsActivity.MODE_INCOME)
             startActivity(intent)
         }
     }
 
-    private fun setExpensesListener() {
+    private fun setOnExpensesClickListener() {
         binding.cvExpenses.setOnClickListener {
             val intent = ExpensesActivity.newIntent(this)
             startActivity(intent)
         }
     }
 
-    private fun setMinusListener() {
+    private fun setOnMinusClickListener() {
         binding.cvMinus.setOnClickListener {
             val intent = OperationsActivity.newIntent(this, OperationsActivity.MODE_EXPENSE)
             startActivity(intent)
         }
     }
 
-    private fun setMoneyBoxListener() {
+    private fun setOnMoneyBoxClickListener() {
         binding.cvMoneyBox.setOnClickListener {
             val intent = MoneyBoxActivity.newIntent(this)
             startActivity(intent)
         }
     }
 
-    private fun setDebtsListener() {
+    private fun setOnDebtsClickListener() {
         binding.cvDebts.setOnClickListener {
             val intent = DebtsActivity.newIntent(this)
             startActivity(intent)
         }
     }
 
-    private fun setLimitsListener() {
+    private fun setOnLimitsClickListener() {
         binding.cvLimits.setOnClickListener {
             val intent = LimitsActivity.newIntent(this)
             startActivity(intent)
         }
     }
 
-    private fun setAlarmsListener() {
+    private fun setOnAlarmsClickListener() {
         binding.cvAlarms.setOnClickListener {
             val intent = AlarmsActivity.newIntent(this)
             startActivity(intent)
         }
     }
 
-    private fun setHistoryListener() {
+    private fun setOnHistoryClickListener() {
         binding.cvHistory.setOnClickListener {
             val intent = HistoryActivity.newIntent(this)
             startActivity(intent)
         }
     }
 
-    private fun setSettingsListener() {
+    private fun setOnSettingsClickListener() {
         binding.cvSettings.setOnClickListener {
             val intent = SettingsActivity.newIntent(this)
             startActivity(intent)
