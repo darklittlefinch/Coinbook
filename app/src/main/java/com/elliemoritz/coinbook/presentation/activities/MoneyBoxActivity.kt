9c97990be9b46c1e7013b33d5c41009e861b45f8
Elisa Moritz <dark.little.finch@gmail.com>
2024-08-01 06:45:19 +0700

    refactor: delete unused import

--- app/src/main/java/com/elliemoritz/coinbook/presentation/activities/MoneyBoxActivity.kt
+++ app/src/main/java/com/elliemoritz/coinbook/presentation/activities/MoneyBoxActivity.kt
@@ -3,7 +3,6 @@ package com.elliemoritz.coinbook.presentation.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
